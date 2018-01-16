411efd1df77a332fb1b8f01cc6847ee096854ec7
Nemanja Pantoš <npantos@gmail.com>
2018-01-16 18:32:15 +0100

    Zadatak 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
+const appRoutes: Routes = [
+    {path: '', component: CarsComponent},
+    {path: 'cars', component: CarsComponent},
+    {path: 'cars/:id', component: CarsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    CarsComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+      RouterModule.forRoot(
+          appRoutes // <-- our constant from previous slide
+      ),
+      BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
